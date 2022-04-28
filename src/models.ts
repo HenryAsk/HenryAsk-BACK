cf9312c496f7a0ec4514896abf8b654f14287648
Daiana <daiana_agustina@outlook.com>
2022-04-27 23:25:25 -0300

    [ADD] User routes & controllers

--- src/models.ts
+++ src/models.ts
@@ -1,11 +1,11 @@
-const mongoose = require('./index'),
-    UserModel = require('./models/User'),
-    TheoricModel = require('./models/Theoric');
+// const mongoose = require('./index'),
+//     UserModel = require('./models/User'),
+//     TheoricModel = require('./models/Theoric');
 
-const models = {
-    User: mongoose.model('user', UserModel),
-    Theoric: mongoose.model('theoric', TheoricModel)
-};
+// const models = {
+//     User: mongoose.model('user', UserModel),
+//     Theoric: mongoose.model('theoric', TheoricModel)
+// };
 
 
-module.exports = models;
+// module.exports = models;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,17 +1,24 @@
-const router = require("express").Router();
+import { Router } from "express";
+const router = Router();
 
 /*IMPORT RUTAS THEORIC*/
-import { postTheoric } from "../controllers/Theoric Controllers/postTheoric";
-import { editTheoric } from "../controllers/Theoric Controllers/editTheoric";
-import { deleteTheoric } from "../controllers/Theoric Controllers/deleteTheoric";
+// import { postTheoric } from "../controllers/Theoric Controllers/postTheoric";
+// import { editTheoric } from "../controllers/Theoric Controllers/editTheoric";
+// import { deleteTheoric } from "../controllers/Theoric Controllers/deleteTheoric";
+
+import { allUsers, userById } from './userRoute';
+
+/*RUTAS GET*/
+router.use("/user", allUsers);
+router.use("/user", userById);
 
 /*RUTAS POST*/
-router.post("/theoric/posttheoric", postTheoric);
+// router.post("/theoric/posttheoric", postTheoric);
 
 /*RUTAS PUT*/
-router.put("/theoric/edittheoric", editTheoric);
+// router.put("/theoric/edittheoric", editTheoric);
 
 /*RUTAS DELETE*/
-router.delete("/theoric/deletetheoric", deleteTheoric);
+// router.delete("/theoric/deletetheoric", deleteTheoric);
 
 module.exports = router;
