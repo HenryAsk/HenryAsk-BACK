785ff22869d9614770227cb2127950d7e1a5b085
Daiana <daiana_agustina@outlook.com>
2022-04-28 19:13:40 -0300

    [ADD] config

--- src/controllers/Users Controllers/editUser.ts
+++ src/controllers/Users Controllers/editUser.ts
@@ -20,21 +20,22 @@ export const EDIT_USER = async (req: Request, res: Response) => {
         if(!id){
             res.status(404).send('Ha ocurrido un error al editar el usuario.')
         }
+        else {
+            const userEdited = await User.updateOne({ _id: id }, {
+                first_name: first_name && first_name,
+                last_name: last_name && last_name,
+                user_name: user_name && user_name,
+                email: email && email,
+                country: country && country,
+                city: city && city,
+                profile_picture: profile_picture && profile_picture,
+                biography: biography && biography,
+                github: github && github,
+                linkedin: linkedin && linkedin
+            });
+            res.status(200).json(`${userEdited.matchedCount} document has been matched and ${userEdited.modifiedCount} document has been modified `);
+        }
 
-        const userEdited = await User.updateOne({ _id: id }, {
-            first_name: first_name && first_name,
-            last_name: last_name && last_name,
-            user_name: user_name && user_name,
-            email: email && email,
-            country: country && country,
-            city: city && city,
-            profile_picture: profile_picture && profile_picture,
-            biography: biography && biography,
-            github: github && github,
-            linkedin: linkedin && linkedin
-        });
-
-        res.status(200).json(`${userEdited.matchedCount} document has been matched and ${userEdited.modifiedCount} document has been modified `);
 
     } catch(err: any | unknown) {
 
