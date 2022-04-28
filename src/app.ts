0b61a7cc180dbdfb4aa281452ebf30492125b0bf
Daiana <daiana_agustina@outlook.com>
2022-04-28 13:24:26 -0300

    [ADD] Config

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ const routes = require('./routes/index');
 const express = require('express');
 const morgan = require('morgan');
 const server = express();
+import { Request, Response, NextFunction } from 'express';
 
 require('./db');
 
@@ -11,6 +12,13 @@ server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
+server.use((req: Request, res: Response, next: NextFunction) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 server.set('port', process.env.PORT || 3004);
 server.use(express.json());
