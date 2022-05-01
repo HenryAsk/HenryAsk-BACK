bcd76e08dec87ba158cf1b2baf8fdad87f2b6081
AlejoBengo <henryaskhenry@gmail.com>
2022-05-01 20:08:47 -0300

    [FIX]

--- src/controllers/TheoricsControllers/getAllTheorics.ts
+++ src/controllers/TheoricsControllers/getAllTheorics.ts
@@ -3,25 +3,32 @@ import { TheoricModel } from "../../models/Theorics";
 
 /*Este es el controller para borrar contenido teórico.*/
 
-export const GET_ALL_THEORICS = async (req: Request, res: Response, next: NextFunction) => {
-
+export const GET_ALL_THEORICS = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const { word } = req.query;
   const { id } = req.params;
 
   if (id || word) next();
   else {
     try {
-      let theoricFounded = await TheoricModel.find({})
-      .populate("owner","_id user_name profile_picture role");
+      let theoricFounded = await TheoricModel.find({}).populate(
+        "owner",
+        "_id user_name profile_picture role"
+      );
 
-      if(!theoricFounded.length){
+      if (!theoricFounded.length) {
         throw new Error("No se ha encontrado ningún contenido teórico");
-        
       }
       res.status(200).json(theoricFounded);
     } catch (err: string | any) {
-
-      res.status(400).json(`Algo salió mal en el controller GET_ALL_THEORICS: ${err.message}`);
+      res
+        .status(400)
+        .json(
+          `Algo salió mal en el controller GET_ALL_THEORICS: ${err.message}`
+        );
     }
   }
 };
--- src/controllers/UsersControllers/editUser.ts
+++ src/controllers/UsersControllers/editUser.ts
@@ -1,56 +1,64 @@
 import { Request, Response } from "express";
 import { uploadImg } from "../../cloudinary";
-const User = require('../../models/Users');
+const User = require("../../models/Users");
 
 export const EDIT_USER = async (req: Request, res: Response) => {
-    try{
-        const {
-            id,
-            first_name,
-            last_name,
-            user_name,
-            email,
-            country,
-            city,
-            role,
-            posts,
-            answers,
-            comments,
-            theorics,
-            exercises,
-            profile_picture,
-            biography,
-            github,
-            linkedin
-        } = req.body;
+  try {
+    const {
+      id,
+      first_name,
+      last_name,
+      user_name,
+      email,
+      country,
+      city,
+      role,
+      posts,
+      answers,
+      comments,
+      theorics,
+      exercises,
+      profile_picture,
+      biography,
+      github,
+      linkedin,
+    } = req.body;
+    console.log(first_name);
+    if (!id) {
+      res
+        .status(404)
+        .json({ error: "Ha ocurrido un error al editar el usuario." });
+    } else {
+      const image = await uploadImg(profile_picture);
 
-        if(!id){
-            res.status(404).send('Ha ocurrido un error al editar el usuario.')
+      const userEdited = await User.updateOne(
+        { _id: id },
+        {
+          first_name: first_name && first_name,
+          last_name: last_name && last_name,
+          user_name: user_name && user_name,
+          email: email && email,
+          country: country && country,
+          city: city && city,
+          role: role && role,
+          posts: posts && posts,
+          answers: answers && answers,
+          comments: comments && comments,
+          theorics: theorics && theorics,
+          exercises: exercises && exercises,
+          profile_picture: image.secure_url,
+          biography: biography && biography,
+          github: github && github,
+          linkedin: linkedin && linkedin,
         }
-        else {
-            const image = await uploadImg(profile_picture);
-
-            const userEdited = await User.updateOne({ _id: id }, {
-                first_name: first_name && first_name,
-                last_name: last_name && last_name,
-                user_name: user_name && user_name,
-                email: email && email,
-                country: country && country,
-                city: city && city,
-                role: role && role,
-                posts: posts && posts,
-                answers: answers && answers,
-                comments: comments && comments,
-                theorics: theorics && theorics,
-                exercises: exercises && exercises,
-                profile_picture: image.secure_url,
-                biography: biography && biography,
-                github: github && github,
-                linkedin: linkedin && linkedin
-            });
-            res.status(200).json(`${userEdited.matchedCount} document has been matched and ${userEdited.modifiedCount} document has been modified `);
-        }
-    } catch(err: any | string) {
-        res.status(404).send(err.message);
+      );
+      res
+        .status(200)
+        .json(
+          `${userEdited.matchedCount} document has been matched and ${userEdited.modifiedCount} document has been modified `
+        );
     }
+  } catch (err: any | string) {
+    res.status(404).send(err.message);
+  }
 };
