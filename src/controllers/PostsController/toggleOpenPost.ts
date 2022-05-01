06c35fb361b6c63343b50dc7a6ebc4019abb954e
AgustinVillagran <magustin.villagran@gmail.com>
2022-05-01 19:07:36 -0300

    [FIXS][ADD] MODEL_USER_EDIT_POST_CONTROLLER__BANNER_ATTRIBUTE

--- src/controllers/PostsController/toggleOpenPost.ts
+++ src/controllers/PostsController/toggleOpenPost.ts
@@ -3,26 +3,25 @@ import { PostModel} from '../../models/Posts';
 
 export const TOGGLE_OPEN_POST = async (req: Request, res: Response, next: NextFunction) => {
   
-  const{ description, question, tags, title} = req.body;
+  const{ description, question, tags } = req.body;
   
-  if( description || question || tags || title ) next()
+  if( description || question || tags ) next()
   else{
     try{
       const {id, open} = req.body;
 
       if(!id){
-          res.status(404).json({
-              error:'Debe ingresar el id del post que desea cerrar.'
-          });
+          throw new Error('Debe ingresar el id del post que desea cerrar.');
       }
 
       const oldPost = await PostModel.updateOne({ _id: id },{
         open: open
       });
-      res.json(`Post cerrado correctamente.`);     
+      console.log("oldPost:", oldPost)
+      res.json(`${oldPost.modifiedCount} post ${open? "abierto":"cerrado"} correctamente.`);     
 
   } catch(err: string | any){
-      res.status(404).send(err.message);
+      res.status(404).send(`Error en el controller TOGGLE_OPEN_POST: ${err.message}`);
   }
   }
   
