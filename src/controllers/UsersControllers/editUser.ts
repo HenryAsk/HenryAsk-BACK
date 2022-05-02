29262701fc8cc476804b7d2838d73b76f4fc5e3e
SaulPaez03 <saulpaez03@gmail.com>
2022-05-01 20:12:44 -0400

    Fix ahora si 100% fix no rompe nada

--- src/controllers/UsersControllers/editUser.ts
+++ src/controllers/UsersControllers/editUser.ts
@@ -29,7 +29,13 @@ export const EDIT_USER = async (req: Request, res: Response) => {
         .status(404)
         .json({ error: "Ha ocurrido un error al editar el usuario." });
     } else {
-      const image = await uploadImg(profile_picture);
+      let image;
+      if (profile_picture !== "") {
+        image = await uploadImg(profile_picture);
+        image = image.secure_url;
+      } else {
+        image = "";
+      }
 
       const userEdited = await User.updateOne(
         { _id: id },
@@ -46,43 +52,21 @@ export const EDIT_USER = async (req: Request, res: Response) => {
           comments: comments && comments,
           theorics: theorics && theorics,
           exercises: exercises && exercises,
-          profile_picture: image.secure_url,
+          profile_picture: image,
           biography: biography && biography,
           github: github && github,
           linkedin: linkedin && linkedin,
         }
-        else {
-            let image;
-            if(profile_picture !== ""){
-                image = await uploadImg(profile_picture);
-                image = image.secure_url
-            } else {
-                image = "";
-            }
-
-            const userEdited = await User.updateOne({ _id: id }, {
-                first_name: first_name && first_name,
-                last_name: last_name && last_name,
-                user_name: user_name && user_name,
-                email: email && email,
-                country: country && country,
-                city: city && city,
-                role: role && role,
-                posts: posts && posts,
-                answers: answers && answers,
-                comments: comments && comments,
-                theorics: theorics && theorics,
-                exercises: exercises && exercises,
-                profile_picture: image,
-                biography: biography && biography,
-                github: github && github,
-                linkedin: linkedin && linkedin
-            });
-            res.status(200).json(`${userEdited.matchedCount} document has been matched and ${userEdited.modifiedCount} document has been modified `);
-        }
-    } catch(err: any | string) {
-        res.status(404).send(err.message);
+      );
+      res
+        .status(200)
+        .json(
+          `${userEdited.matchedCount} document has been matched and ${userEdited.modifiedCount} document has been modified `
+        );
     }
+    // } catch(err: any | string) {
+    //     res.status(404).send(err.message);
+    // }
   } catch (err: any | string) {
     res.status(404).send(err.message);
   }
