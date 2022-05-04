0cfebc839ba0e8e4f78353a2ff401480093b276a
Daiana <daiana_agustina@outlook.com>
2022-05-03 22:02:01 -0300

    [FIX]

--- src/controllers/UsersControllers/getByMailUser.ts
+++ src/controllers/UsersControllers/getByMailUser.ts
@@ -2,40 +2,43 @@ import { NextFunction, Request, Response } from "express";
 const User = require("../../models/Users");
 
 export const GET_USER_BY_MAIL = async ( req: Request, res: Response, next: NextFunction ) => {
+  if(req.query.user_name) next();
+
+  else{
+    try {
+      const { email } = req.query;
   
-  try {
-    const { email } = req.query;
-
-    if (!email) next();
-
-    if (email) {
-      let userByMail = await User.findOne({ email: email });
-
-      if (userByMail) {
-        userByMail = {
-          _id: userByMail._id,
-          first_name: userByMail.first_name,
-          last_name: userByMail.last_name,
-          user_name: userByMail.user_name,
-          email: userByMail.email,
-          role: userByMail.role,
-          country: userByMail.country,
-          city: userByMail.city,
-          profile_picture: userByMail.profile_picture,
-          banner: userByMail.banner,
-          biography: userByMail.biography,
-          github: userByMail.github,
-          linkedin: userByMail.linkedin,
-          own_henry_coin: userByMail.own_henry_coin,
-          give_henry_coin: userByMail.give_henry_coin,
-        };
-
-        res.status(200).json(userByMail);
-      } else {
-        res.status(404).send("No se encontró el usuario requerido.");
+      if (!email) next();
+  
+      if (email) {
+        let userByMail = await User.findOne({ email: email });
+  
+        if (userByMail) {
+          userByMail = {
+            _id: userByMail._id,
+            first_name: userByMail.first_name,
+            last_name: userByMail.last_name,
+            user_name: userByMail.user_name,
+            email: userByMail.email,
+            role: userByMail.role,
+            country: userByMail.country,
+            city: userByMail.city,
+            profile_picture: userByMail.profile_picture,
+            banner: userByMail.banner,
+            biography: userByMail.biography,
+            github: userByMail.github,
+            linkedin: userByMail.linkedin,
+            own_henry_coin: userByMail.own_henry_coin,
+            give_henry_coin: userByMail.give_henry_coin,
+          };
+  
+          res.status(200).json(userByMail);
+        } else {
+          res.status(404).send("No se encontró el usuario requerido.");
+        }
       }
+    } catch (err: any | unknown) {
+      res.status(404).send(err.message);
     }
-  } catch (err: any | unknown) {
-    res.status(404).send(err.message);
   }
 };
--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -9,8 +9,8 @@ import { GET_BY_USER_NAME } from "../controllers/UsersControllers/getByUserName"
 const router = Router();
 
 //NO CAMBIAR EL ORDEN: PRIMERO LAS DINÁMICAS//
-router.get('/', GET_BY_USER_NAME);
 router.get('/', GET_USER_BY_MAIL);
+router.get('/', GET_BY_USER_NAME);
 router.get('/', GET_ALL_USER);
 router.get('/:id', GET_USER_BY_ID);
 router.put('/', EDIT_USER );
